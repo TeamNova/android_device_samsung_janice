2bef874c6dfd6fac8e6a2bc39e5f1c276bfbe75c
iiD4x <t2pksa@gmail.com>
2014-04-14 20:27:28 +0300

    Change Vendor directory

--- janice.mk
+++ janice.mk
@@ -44,4 +44,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # Use non-open-source parts if present
-$(call inherit-product-if-exists, vendor/samsung/u8500-common/janice/janice-vendor-blobs.mk)
+$(call inherit-product-if-exists, vendor/samsung/janice/janice-vendor-blobs.mk)
